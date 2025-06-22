3507613c883c94f43b3cf49c2821c817ef1445b0
Ubalasiya <skylifecommunity0@gmail.com>
2025-06-22 21:49:51 +0530

    Add files via upload

---
+++ plugins/alive.js
@@ -0,0 +1,52 @@
+
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+
+cmd({
+    pattern: "alive",
+    alias: ["status", "runtime", "uptime"],
+    desc: "Check uptime and system status",
+    category: "main",
+    react: "📟",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        // Generate system status message
+        const status = `┏━❮ 🩵𝗖𝗛𝗔𝗠𝗜 𝗗𝗘𝗧𝗔𝗜𝗟𝗦🩵 ❯━
+┃◈┃🤖 ʙᴏᴛ ɴᴀᴍᴇ :𝐂𝐇𝐀𝐌𝐈-𝐌𝐃
+┃◈┃🔖 ᴠᴇʀsɪᴏɴ : 1.0
+┃◈┃📟 ᴘʟᴀᴛғᴏʀᴍ : Linux
+┃◈┃👨‍💻ᴏᴡɴᴇʀ: 𝐌𝐑 𝐂𝐇𝐀𝐌𝐎𝐃
+┃◈┃📆 ʀᴜɴᴛɪᴍᴇ : ${runtime(process.uptime())} 
+┃◈┃📈ʀᴀᴍ ᴜsᴀɢᴇ: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+┃◈┗━━━━━━━━━━━━━━𖣔𖣔
+╰──────────────┈⊷
+> 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘾𝙃𝘼𝙈𝙄-𝙈𝘿`;
+
+        // Send the status message with an image
+        await conn.sendMessage(from, { 
+            image: { url: `https://raw.githubusercontent.com/Ubalasiya/Chamihelper/refs/heads/main/chami-md-main.jpg` },  // Image URL
+            caption: status,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '',
+                    newsletterName: '𝘾𝙃𝘼𝙈𝙄',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Error in alive command:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
+
+//𝙲𝚁𝚁𝙰𝚃𝙴 𝙱𝚈 𝙼𝚁 𝙻𝙰𝙺𝚂𝙸𝙳𝚄
+// 𝙻𝙰𝙺𝙸𝚈𝙰 𝙼𝙳 
+//𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝙰𝙻𝙸𝚅𝙴 𝙿𝙻𝚄𝙶𝙸𝙽
