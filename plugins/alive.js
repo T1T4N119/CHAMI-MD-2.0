9df7c009e8f49d69c58a700c84e5f12bd8c5ec60
Ubalasiya <skylifecommunity0@gmail.com>
2025-06-22 22:11:30 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,6 +1,7 @@
-const { cmd, commands } = require('../command');
-const os = require("os");
-const { runtime } = require('../lib/functions');
+
+const l = console.log
+const config = require('../settings')
+const { cmd, commands } = require('../lib/command')
 
 cmd({
     pattern: "alive",
@@ -13,28 +14,28 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `┏━❮ 🩵𝐋𝐀𝐊𝐈𝐘𝐀 𝐃𝐄𝐓𝐀𝐋𝐄𝐒🩵 ❯━
-┃◈┃🤖 ʙᴏᴛ ɴᴀᴍᴇ :𝐋𝐀𝐊𝐈𝐘𝐀
-┃◈┃🔖 ᴠᴇʀsɪᴏɴ : 2.0
+        const status = `┏━❮ 🩵𝗖𝗛𝗔𝗠𝗜 𝗗𝗘𝗧𝗔𝗜𝗟𝗦🩵 ❯━
+┃◈┃🤖 ʙᴏᴛ ɴᴀᴍᴇ :𝐂𝐇𝐀𝐌𝐈-𝐌𝐃
+┃◈┃🔖 ᴠᴇʀsɪᴏɴ : 1.0
 ┃◈┃📟 ᴘʟᴀᴛғᴏʀᴍ : Linux
-┃◈┃👨‍💻ᴏᴡɴᴇʀ: 𝐌𝐑 𝐋𝐀𝐊𝐒𝐈𝐃𝐔
+┃◈┃👨‍💻ᴏᴡɴᴇʀ: 𝐌𝐑 𝐂𝐇𝐀𝐌𝐎𝐃
 ┃◈┃📆 ʀᴜɴᴛɪᴍᴇ : ${runtime(process.uptime())} 
 ┃◈┃📈ʀᴀᴍ ᴜsᴀɢᴇ: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 ┃◈┗━━━━━━━━━━━━━━𖣔𖣔
 ╰──────────────┈⊷
-> 𝛲𝛩𝑊𝛯𝑅𝐷 𝐵𝑌 𝐿𝛥𝛫𝛪𝑌𝛥 𝛭𝐷`;
+> 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘾𝙃𝘼𝙈𝙄-𝙈𝘿`;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `𝐈𝐀𝐌𝐆𝐄 𝐔𝐑𝐋` },  // Image URL
+            image: { url: `https://raw.githubusercontent.com/Ubalasiya/Chamihelper/refs/heads/main/chami-md-main.jpg` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: 'ᑕ𝐇𝐀𝐍𝐄𝐋 𝐉𝐈𝐃',
-                    newsletterName: '𝛭𝑅 𝐿𝛥𝛫𝑆𝛪𝐷𝑈',
+                    newsletterJid: '',
+                    newsletterName: '𝘾𝙃𝘼𝙈𝙄',
                     serverMessageId: 143
                 }
             }
