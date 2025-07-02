f187aa94aab14ba3ae27fe79d53aaea6a7320c01
𝗖𝗛𝗔𝗠𝗜𝗬𝗔𝗛𝗛...! <skylifecommunity0@gmail.com>
2025-07-02 21:05:35 +0530

    Update china.js

--- plugins/china.js
+++ plugins/china.js
@@ -1,4 +1,4 @@
-const { cmd, commands } = require('../command')
+const { cmd, commands } = require('../lib/command')
 const { fetchJson } = require('../lib/functions');
 var cants = "I cant find this Image."
 
