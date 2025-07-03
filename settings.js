1902afdc148c3ca021bc80de99f7970799a2d64d
𝗖𝗛𝗔𝗠𝗜𝗬𝗔𝗛𝗛...! <skylifecommunity0@gmail.com>
2025-07-03 15:09:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
 module.exports = {
     SESSION_ID: process.env.SESSION_ID === undefined ? 'oQhygbbB#OOQ_ISaYztwNzRgaX_zpbYtaEo-kMHpE1now4X4gLzM' : process.env.SESSION_ID,
     OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94766315540' : process.env.OWNER_NUMBER,
