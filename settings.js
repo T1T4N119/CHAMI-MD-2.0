541126f1bf9d7c164a5a7de04724a4e8c7c57f20
T1T4N119 <pududasuka625@gmail.com>
2025-07-12 18:28:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,5 @@ module.exports = {
     AUTO_STICKER: process.env.AUTO_STICKER || "true",
     AUTO_REPLY: process.env.AUTO_REPLY || "true",
     WEATHER_API_KEY: process.env.WEATHER_API_KEY || "9ad6e2bc255f629e9ff07569f0ad0af3",
-    ANTI_DELETE: process.env.ANTI_DELETE || "true"  
-    AUTO_TYPING: process.env.AUTO_TYPING || "true",
-    AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+    ANTI_DELETE: process.env.ANTI_DELETE || "true"      
 };
