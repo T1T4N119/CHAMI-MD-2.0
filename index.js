309ebd7132e299e9e727630eed6297b2f60ae91d
T1T4N119 <pududasuka625@gmail.com>
2025-07-13 14:53:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -333,6 +333,28 @@ conn.ev.on('messages.update', async(mes) => {
     console.error("AntiDelete Error:", err);
   }
 });     
+
+if (!mek.message) return	
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])  
+const mnyako = await jidNormalizedUser(conn.user.id)
+await conn.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '🧡'}}, { statusJidList: [mek.key.participant, mnyako] })
+}	      
+	    if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+            const m = sms(conn, mek)
+	          var smg = m
+            const type = getContentType(mek.message)
+            const content = JSON.stringify(mek.message)
+            const from = mek.key.remoteJid
+
+> https://whatsapp.com/channel/0029Vb5sh47IHphFvhi7BL2n/386	    
+	      
+	      
+	      
+	    
+	      
+	      
 	    mek = mek.messages[0]
             if (!mek.message) return
 	    var id_db = require('./lib/id_db')    
