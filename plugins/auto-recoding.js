4956c1f24f6f8fed77d3f680ed147c4d1093882d
T1T4N119 <pududasuka625@gmail.com>
2025-07-14 15:13:15 +0530

    Update auto-recoding.js

--- plugins/auto-recoding.js
+++ plugins/auto-recoding.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../settings')
-const {cmd , commands} = require('../lib/command')
+const config = require('../settings');
+const { cmd } = require('../lib/command');
 
-
-//auto recording
+//auto_voice
 cmd({
   on: "body"
 },    
-async (conn, mek, m, { from, body, isOwner }) => {       
- if (config.AUTO_RECORDING === 'true') {
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../assets/autovoice.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_VOICE === 'true') {
+                //if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
+                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
             }
-         } 
-   );
+        }
+    }                
+});
