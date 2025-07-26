16be7f497a4fa52b9394d8bd001c3d83b2e854b0
T1T4N119 <pududasuka625@gmail.com>
2025-07-26 17:53:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -335,10 +335,20 @@ conn.ev.on('messages.update', async(mes) => {
 });     
 	    mek = mek.messages[0]
             if (!mek.message) return
-	    var id_db = require('./lib/id_db')    
             mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-            const m = sms(conn, mek)
-	    var smg = m
+
+
+//=====================AUTOStatusview==============//	    
+	     if (!mek.message) return	
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])  
+const mnyako = await jidNormalizedUser(conn.user.id)
+await conn.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '🧡'}}, { statusJidList: [mek.key.participant, mnyako] })
+}	      
+	    if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+            const m = sms(conn, mek)                  
+            var id_db = require('./lib/id_db')
             const type = getContentType(mek.message)
             const content = JSON.stringify(mek.message)
             const from = mek.key.remoteJid
